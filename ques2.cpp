f0edcfeb6aec9713c6c321b42afdffdf56698af5
25110CN230Anurag <anuragyadav29072007@gmail.com>
2026-06-01 17:41:26 +0530

    Add files via upload

---
+++ ques2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n; cin>>n;
+    int number;
+    cin>>number;
+    for(int i =0; i<n; i++){
+       int  ans = number*i;
+       cout<<ans<<endl;
+    }
+    return 0;
+}
